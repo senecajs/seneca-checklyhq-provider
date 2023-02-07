52eadebc764cb0546b7061961f512c9b73898180
José Perigolo Neto <neto.perigolo@gmail.com>
2023-02-07 06:16:51 -0300

    Moved utils functions
    Moved utils functions to another repository,
    Moved types inside provider file

--- src/checklyhq-provider.ts
+++ src/checklyhq-provider.ts
@@ -1,131 +1,123 @@
 /* Copyright © 2022 Seneca Project Contributors, MIT License. */
-import { ChecklyhqProviderOptions } from './types'
-import { getJSON, makeConfig, makeUrl, postJSON } from './utils'
+import fetch from "node-fetch";
 
-const Pkg = require('../package.json')
+const Pkg = require("../package.json");
+
+type ChecklyhqProviderOptions = {
+  url: string
+  fetch?: any
+  debug: boolean
+}
 
 function ChecklyhqProvider(this: any, options: ChecklyhqProviderOptions) {
-  const seneca: any = this
+  const seneca: any = this;
 
-  const entityBuilder = this.export('provider/entityBuilder')
+  const entityBuilder = this.export("provider/entityBuilder");
 
-  seneca.message('sys:provider,provider:checklyhq,get:info', get_info)
+  const makeUrl = this.export("provider/makeUrl");
+  const makeConfig = this.export("provider/makeConfig");
+  const getJSON = this.export("provider/getJSON");
+  const postJSON = this.export("provider/postJSON");
+  const deleteJSON = this.export("provider/deleteJSON");
+
+  seneca.message("sys:provider,provider:checklyhq,get:info", get_info);
 
   async function get_info(this: any, _msg: any) {
     return {
       ok: true,
-      name: 'checklyhq',
+      name: "checklyhq",
       version: Pkg.version,
-      mark: 'checklyhq-provider',
-    }
+      mark: "checklyhq-provider",
+    };
   }
 
   entityBuilder(this, {
     provider: {
-      name: 'checklyhq'
+      name: "checklyhq",
     },
     entity: {
       checks: {
         cmd: {
           list: {
-            action: async function(this: any, entize: any, msg: any) {
-              const res: any = await getJSON(makeUrl('checks', msg.q, options), options, makeConfig(this))
-              let checks = res
-              let list = checks.map((data: any) => entize(data))
-              
-              return list
+            action: async function (this: any, entize: any, msg: any) {
+              const res: any = await getJSON(makeUrl("checks", msg.q, options), options, makeConfig(this));
+              const checks = [...res]
+              const list = checks.map((data: any) => entize(data));
+
+              return list;
             },
           },
           load: {
-            action: async function(this: any, entize: any, msg: any) {
-              const res: any = await getJSON(makeUrl('checks', msg.q, options), options, makeConfig(this))
-              let load = res ? entize(res) : null
+            action: async function (this: any, entize: any, msg: any) {
+              const res: any = await getJSON(makeUrl("checks", msg.q.id, options), options, makeConfig(this));
+              const load = res ? entize(res) : null;
 
-              return load
+              return load;
             },
           },
           save: {
-            action: async function(this: any, entize: any, msg: any) {
-              const body = this.util.deep(
-                options.entity.checks.save,
-                msg.ent.data$(false)
-                )
-              console.log(options)
-              console.log(body)
+            action: async function (this: any, entize: any, msg: any) {
+              //These three properties are required to create a check
+              const body = { name: msg.ent.name, request: msg.ent.request, locations: msg.ent.locations };
 
-              const res: any = await postJSON(makeUrl('checks/api', msg.q, options), makeConfig(this,{
-                body
-              }), options)
+              const res: any = await postJSON(
+                makeUrl("checks/api", msg.q, options),
+                options,
+                makeConfig(this, {
+                  body,
+                }),
+              );
 
-              const project = res
-              return entize(project)
+              const save = res ? entize(res) : null;
+
+              return entize(save);
             },
+          },
+          remove: {
+            action: async function (this: any, entize: any, msg: any) {
+              const res : any = await deleteJSON( makeUrl("checks", msg.q.id, options), options, makeConfig(this))
+              const remove = res ? entize(res) : null;
+
+              return entize(remove);
+            }
           }
-        }
-      }
-    }
-  })
+        },
+      },
+    },
+  });
 
-
-
-  seneca.prepare(async function(this: any) {
-    let res = await this.post('sys:provider,get:keymap,provider:checklyhq')
+  seneca.prepare(async function (this: any) {
+    let res = await this.post("sys:provider,get:keymap,provider:checklyhq");
 
     if (!res.ok) {
-      throw this.fail('keymap')
+      throw this.fail("keymap");
     }
 
-    const auth = res.keymap.token.value
+    const auth = res.keymap.token.value;
 
     this.shared.headers = {
-      Authorization: 'Bearer ' + auth,
-      "X-Checkly-Account": res.keymap.account.value
-    }
-
-    // this.shared.primary = { 
-    //   name: res.keymap.checkname.value,
-    // }
-
-  })
-
-
-  return {
-    exports: {
-      makeUrl,
-      makeConfig,
-      getJSON,
-      postJSON,
-    }
-  }
+      Authorization: "Bearer " + auth,
+      "X-Checkly-Account": res.keymap.account_id.value,
+    };
+  });
 }
-
 
 // Default options.
 const defaults: ChecklyhqProviderOptions = {
-
   // Vercel checks API Endpoint /
-  url: 'https://api.checklyhq.com/v1/',
-  
-  // Use global fetch by default - if exists
-  fetch: ('undefined' === typeof fetch ? undefined : fetch),
+  url: "https://api.checklyhq.com/v1/",
 
-  entity: {
-    checks: {
-      save: {
-        // Default fields
-      }
-    }
-  },
+  // Use global fetch by default - if exists
+  fetch: "undefined" === typeof fetch ? undefined : fetch,
 
   // TODO: Enable debug logging
-  debug: false
-}
+  debug: false,
+};
 
+Object.assign(ChecklyhqProvider, { defaults });
 
-Object.assign(ChecklyhqProvider, { defaults })
+export default ChecklyhqProvider;
 
-export default ChecklyhqProvider
-
-if ('undefined' !== typeof (module)) {
-  module.exports = ChecklyhqProvider
+if ("undefined" !== typeof module) {
+  module.exports = ChecklyhqProvider;
 }
--- src/types.ts
+++
@@ -1,6 +0,0 @@
-export type ChecklyhqProviderOptions = {
-    url: string
-    fetch: any
-    entity: Record<string, any>
-    debug: boolean
-}
