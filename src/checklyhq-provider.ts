31efef81c437ef89c9089d4bac9134df32f9e49d
José Perigolo Neto <neto.perigolo@gmail.com>
2023-02-15 09:56:13 -0300

    Removed semicolons

--- src/checklyhq-provider.ts
+++ src/checklyhq-provider.ts
@@ -1,7 +1,7 @@
 /* Copyright © 2022 Seneca Project Contributors, MIT License. */
-import fetch from "node-fetch";
+import fetch from "node-fetch"
 
-const Pkg = require("../package.json");
+const Pkg = require("../package.json")
 
 type ChecklyhqProviderOptions = {
   url: string
@@ -10,17 +10,17 @@ type ChecklyhqProviderOptions = {
 }
 
 function ChecklyhqProvider(this: any, options: ChecklyhqProviderOptions) {
-  const seneca: any = this;
+  const seneca: any = this
 
-  const entityBuilder = this.export("provider/entityBuilder");
+  const entityBuilder = this.export("provider/entityBuilder")
 
-  const makeUrl = this.export("provider/makeUrl");
-  const makeConfig = this.export("provider/makeConfig");
-  const getJSON = this.export("provider/getJSON");
-  const postJSON = this.export("provider/postJSON");
-  const deleteJSON = this.export("provider/deleteJSON");
+  const makeUrl = this.export("provider/makeUrl")
+  const makeConfig = this.export("provider/makeConfig")
+  const getJSON = this.export("provider/getJSON")
+  const postJSON = this.export("provider/postJSON")
+  const deleteJSON = this.export("provider/deleteJSON")
 
-  seneca.message("sys:provider,provider:checklyhq,get:info", get_info);
+  seneca.message("sys:provider,provider:checklyhq,get:info", get_info)
 
   async function get_info(this: any, _msg: any) {
     return {
@@ -28,7 +28,7 @@ function ChecklyhqProvider(this: any, options: ChecklyhqProviderOptions) {
       name: "checklyhq",
       version: Pkg.version,
       mark: "checklyhq-provider",
-    };
+    }
   }
 
   entityBuilder(this, {
@@ -40,25 +40,25 @@ function ChecklyhqProvider(this: any, options: ChecklyhqProviderOptions) {
         cmd: {
           list: {
             action: async function (this: any, entize: any, msg: any) {
-              const res: any = await getJSON(makeUrl("checks", msg.q, options), options, makeConfig(this));
+              const res: any = await getJSON(makeUrl("checks", msg.q, options), options, makeConfig(this))
               const checks = [...res]
-              const list = checks.map((data: any) => entize(data));
+              const list = checks.map((data: any) => entize(data))
 
-              return list;
+              return list
             },
           },
           load: {
             action: async function (this: any, entize: any, msg: any) {
-              const res: any = await getJSON(makeUrl("checks", msg.q.id, options), options, makeConfig(this));
-              const load = res ? entize(res) : null;
+              const res: any = await getJSON(makeUrl("checks", msg.q.id, options), options, makeConfig(this))
+              const load = res ? entize(res) : null
 
-              return load;
+              return load
             },
           },
           save: {
             action: async function (this: any, entize: any, msg: any) {
               //These three properties are required to create a check
-              const body = { name: msg.ent.name, request: msg.ent.request, locations: msg.ent.locations };
+              const body = { name: msg.ent.name, request: msg.ent.request, locations: msg.ent.locations }
 
               const res: any = await postJSON(
                 makeUrl("checks/api", msg.q, options),
@@ -66,40 +66,40 @@ function ChecklyhqProvider(this: any, options: ChecklyhqProviderOptions) {
                 makeConfig(this, {
                   body,
                 }),
-              );
+              )
 
-              const save = res ? entize(res) : null;
+              const save = res ? entize(res) : null
 
-              return entize(save);
+              return entize(save)
             },
           },
           remove: {
             action: async function (this: any, entize: any, msg: any) {
               const res : any = await deleteJSON( makeUrl("checks", msg.q.id, options), options, makeConfig(this))
-              const remove = res ? entize(res) : null;
+              const remove = res ? entize(res) : null
 
-              return entize(remove);
+              return entize(remove)
             }
           }
         },
       },
     },
-  });
+  })
 
   seneca.prepare(async function (this: any) {
-    let res = await this.post("sys:provider,get:keymap,provider:checklyhq");
+    let res = await this.post("sys:provider,get:keymap,provider:checklyhq")
 
     if (!res.ok) {
-      throw this.fail("keymap");
+      throw this.fail("keymap")
     }
 
-    const auth = res.keymap.token.value;
+    const auth = res.keymap.token.value
 
     this.shared.headers = {
       Authorization: "Bearer " + auth,
       "X-Checkly-Account": res.keymap.account_id.value,
-    };
-  });
+    }
+  })
 }
 
 // Default options.
@@ -112,12 +112,12 @@ const defaults: ChecklyhqProviderOptions = {
 
   // TODO: Enable debug logging
   debug: false,
-};
+}
 
-Object.assign(ChecklyhqProvider, { defaults });
+Object.assign(ChecklyhqProvider, { defaults })
 
-export default ChecklyhqProvider;
+export default ChecklyhqProvider
 
 if ("undefined" !== typeof module) {
-  module.exports = ChecklyhqProvider;
+  module.exports = ChecklyhqProvider
 }
